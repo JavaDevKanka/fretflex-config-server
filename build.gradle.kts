f451b9629ef98a761bb316fa5735c0d181df387c
bidlokoder <daniil.sakhnov@rosbank.ru>
2024-01-21 18:58:08 +0300

    edit project name

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "ru.konkatenazia.config-server"
+group = "ru.fretflex.config-server"
 version = "0.0.1-SNAPSHOT"
 
 java {
